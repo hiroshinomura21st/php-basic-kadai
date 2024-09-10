6ca8d6960e6843121c0dcbd5f46725734da5f9a7
hiroshinomura21st <158221628+hiroshinomura21st@users.noreply.github.com>
2024-09-10 15:32:14 +0900

    kadai_007ファイルのアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCUTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編_7章課題</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $vegitable_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+        print_r($vegitable_data);
+        ?>
+    </p>
+</body>
+</html>
